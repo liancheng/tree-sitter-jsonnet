d25336b7036526298e3330dfe76d0b3c9708739d
Cheng Lian <lian@databricks.com>
2026-07-09 12:37:40 -0700

    Add test cases in the Rust binding

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -4,8 +4,7 @@
 //! tree-sitter [`Parser`], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = r#"
-//! "#;
+//! let code = r#"1"#;
 //! let mut parser = tree_sitter::Parser::new();
 //! let language = tree_sitter_jsonnet::LANGUAGE;
 //! parser
@@ -50,11 +49,86 @@ pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_can_load_grammar() {
-        let mut parser = tree_sitter::Parser::new();
+    use tree_sitter::{self as T};
+
+    fn parser() -> T::Parser {
+        let mut parser = T::Parser::new();
         parser
             .set_language(&super::LANGUAGE.into())
             .expect("Error loading Jsonnet parser");
+        parser
+    }
+
+    fn assert_node_eq(
+        node: T::Node,
+        kind: &str,
+        start: impl Into<Option<(usize, usize)>>,
+        end: impl Into<Option<(usize, usize)>>,
+    ) {
+        assert_eq!(node.kind(), kind);
+
+        if let Some((row, column)) = start.into() {
+            assert_eq!(node.start_position(), T::Point::new(row, column));
+        }
+
+        if let Some((row, column)) = end.into() {
+            assert_eq!(node.end_position(), T::Point::new(row, column));
+        }
+    }
+
+    #[test]
+    fn test_parse_number() {
+        let tree = parser().parse("1", None).unwrap();
+        let root = tree.root_node();
+        assert!(!root.has_error());
+
+        assert_eq!(root.kind(), "document");
+        assert_eq!(root.child_count(), 1);
+
+        let number = root.child(0).unwrap();
+        assert_eq!(number.kind(), "number");
+        assert_node_eq(number, "number", (0, 0), (0, 1));
+    }
+
+    #[test]
+    fn test_parse_text_block() {
+        let source = indoc::indoc! {"
+            |||
+              $
+                c
+            |||
+        "}
+        .replace("$", "");
+
+        let tree = parser().parse(source, None).unwrap();
+        let root = tree.root_node();
+        assert!(!root.has_error());
+
+        assert_eq!(root.kind(), "document");
+        assert_eq!(root.child_count(), 1);
+
+        let string = root.child(0).unwrap();
+        assert_eq!(string.kind(), "string");
+
+        let text_block = string.child(0).unwrap();
+
+        assert_node_eq(text_block, "text_block", (0, 0), (3, 3));
+
+        assert_eq!(text_block.child(0).unwrap().kind(), "text_block_start");
+        assert_eq!(text_block.child(3).unwrap().kind(), "text_block_end");
+
+        assert_node_eq(
+            text_block.child(1).unwrap(),
+            "text_block_line",
+            (1, 0),
+            (2, 0),
+        );
+
+        assert_node_eq(
+            text_block.child(2).unwrap(),
+            "text_block_line",
+            (2, 0),
+            (3, 0),
+        );
     }
 }
